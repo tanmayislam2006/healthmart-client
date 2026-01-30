2fd5f547452d6034eee8f0eed36af8570afa9a24
tanmay2006 <tanmayislam101@gmail.com>
2026-01-31 01:45:58 +0600

    added type safty for env file

--- src/app/(commonLayout)/sign-up/page.tsx
+++ src/app/(commonLayout)/sign-up/page.tsx
@@ -1,7 +1,10 @@
 import { SignupForm } from "@/components/signup-form";
 
 
+
+
 export default function SignUpPage() {
+  
   return (
     <div className="flex  w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
---
+++ src/env.ts
@@ -0,0 +1,23 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    BACKEND_URL: z.string().url(),
+    FRONTEND_URL: z.string().url(),
+    API_URL: z.string().url(),
+    AUTH_URL: z.string().url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    API_URL: process.env.API_URL,
+    AUTH_URL: process.env.AUTH_URL,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
+});
